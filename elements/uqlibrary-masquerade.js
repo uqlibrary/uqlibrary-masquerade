3da9cd9ce308cb4ac36fa978b41f593f800f974d
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2016-09-30 07:52:53 +1000

    Change to new auth

--- elements/uqlibrary-masquerade.js
+++ elements/uqlibrary-masquerade.js
@@ -66,7 +66,7 @@
 			
 			//first character has to be a string (student number is valid, but can't get get all data, so use user id)
 			if (!parseInt(this._masqueradeValue[0])) {
-				var url = 'https://www.library.uq.edu.au/uqlais/masquerade?' +
+				var url = 'https://auth.library.uq.edu.au/masquerade?' +
 						'user=' + this._masqueradeValue + '&return=' + window.btoa(window.location.href);
 
 				document.cookie = 'UQLMasquerade=' + this._masqueradeValue;
