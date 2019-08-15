b5944987fffec6df06d2f11486303f2fdabeaf6f
Ashley Wilson <Ashley.Wilson@uq.edu.au>
2019-08-15 10:18:09 +1000

    seed gh-pages

---
+++ index.md
@@ -0,0 +1,30 @@
+# uqlibrary-masquerade
+
+[![Dependency Status](https://david-dm.org/uqlibrary/uqlibrary-masquerade.svg)](https://david-dm.org/uqlibrary/uqlibrary-masquerade)
+[![Dev Dependency Status](https://david-dm.org/uqlibrary/uqlibrary-masquerade/dev-status.svg)](https://david-dm.org/uqlibrary/uqlibrary-masquerade?type=dev)
+
+* Full application is [uqlibrary-mylibrary repo](https://github.com/uqlibrary/uqlibrary-mylibrary/).
+* Full documentation can be found [here](https://uqlibrary.github.io/uqlibrary-masquerade/uqlibrary-masquerade/).
+* Demo is [here](https://uqlibrary.github.io/uqlibrary-masquerade/uqlibrary-masquerade/demo/).
+
+## Getting Started
+
+```sh
+npm install -g web-component-tester bower polymer-cli
+npm install
+bower install
+```
+
+## Developing
+
+* Please adhere to the Polymer code style guide provided at [Style Guide](http://polymerelements.github.io/style-guide/).
+* Colors and common styles are imported (bower install) from [uqlibrary-styles](http://github.com/uqlibrary/uqlibrary-styles).
+* GitHub pages should be updated after every commit to `polymer1.0` branch by running `bin/generate-gh-pages.sh`
+
+## Testing
+
+Tests are run using Web Component Tester.
+
+```sh
+npm test
+```
---
+++ uqlibrary-masquerade/elements/uqlibrary-masquerade.js
@@ -0,0 +1,84 @@
+/**
+ * By Jan-Willem Wisgerhof <j.wisgerhof@uq.edu.au>
+ */
+(function () {
+	Polymer({
+		is: 'uqlibrary-masquerade',
+		properties: {
+			/**
+			 * Whether to show the hamburger menu
+			 */
+			standAlone: {
+				type: Object,
+				value: true
+			},
+			/**
+			 * Whether the Masquerade app should auto load the API
+			 * @type Boolean
+			 */
+			autoLoad: {
+				type: Object,
+				value: true
+			},
+			/**
+			 * header title - application name
+			 */
+			headerTitle: {
+				type: String,
+				value: 'Masquerade'
+			},
+		  	/**
+			 * The entered value
+		   	 */
+			_masqueradeValue: {
+				type: String,
+				value: ''
+		  	},
+		  	/**
+		   	 * Holds the user object
+		   	 */
+		  	_account: {
+				type: Object,
+				value: {
+			  		hasSession: false
+				}
+      		}
+		},
+		ready: function () {
+			var self = this;
+			this.$.accountApi.addEventListener('uqlibrary-api-account-loaded', function (e) {
+				if (e.detail.hasSession) {
+					self._account = e.detail;
+				} else {
+					self.$.accountApi.login(document.location.href);
+				}
+
+        self.fire("uqlibrary-masquerade-loaded");
+			});
+
+			this.$.accountApi.get();
+		},
+    /**
+     * Called when the user clicks the masquerade button
+     * @private
+     */
+    _masquerade: function () {
+			
+			//first character has to be a string (student number is valid, but can't get get all data, so use user id)
+			if (!parseInt(this._masqueradeValue[0])) {
+				var url = 'https://auth.library.uq.edu.au/masquerade?' +
+						'user=' + this._masqueradeValue + '&return=' + window.btoa(window.location.href);
+
+				document.cookie = 'UQLMasquerade=' + this._masqueradeValue + ";domain=.library.uq.edu.au;path=/";
+				window.location.href = url;
+			}
+    },
+		/**
+		 * Toggles the drawer panel of the main UQL app
+		 * @private
+		 */
+		_toggleDrawerPanel: function () {
+			this.fire('uqlibrary-toggle-drawer');
+		}
+	})
+})();
